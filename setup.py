900bad222c5faf964d9ba79e45962abea845cf03
Frederik Vaassen <frederik.vaassen@stepstone.be>
2016-06-03 10:43:27 +0200

    Preparing lesson one

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='dl',
+    version='0.1.0',
+    packages=['lesson1'],
+    url='https://classroom.udacity.com/courses/ud730/',
+    license='',
+    author='',
+    author_email='',
+    description='Deeplearning course assignments',
+    install_requires=[
+        'numpy>=1.11.0',
+        'matplotlib>=1.5.1'
+    ]
+)
